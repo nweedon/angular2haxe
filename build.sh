ad0d75c758cde0c5f10d3f4e172387aea039ee24
Niall Weedon <niallweedon@gmail.com>
2015-08-22 19:57:26 +0100

    Download and Install Haxe 3.2 in before_install

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env sh
-haxe -debug --each -cp src -js ./bin/Angular2-Haxe.js -D using_registry -D debug_level=ERROR -main Main -dce full
-mocha runTests.js -t 5000
+~/haxe3.2/haxe -debug --each -cp src -js ./bin/Angular2-Haxe.js -D using_registry -D debug_level=ERROR -main Main -dce full
+mocha spec/runTests.js -t 5000
