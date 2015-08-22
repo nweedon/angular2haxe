a61dec6f8381115c2590728c2e1e43b28b852d1a
Niall Weedon <niallweedon@gmail.com>
2015-08-22 19:59:41 +0100

    Fix Haxe extract path

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env sh
-~/haxe3.2/haxe -debug --each -cp src -js ./bin/Angular2-Haxe.js -D using_registry -D debug_level=ERROR -main Main -dce full
+~/haxe-3.2.0/haxe -debug --each -cp src -js ./bin/Angular2-Haxe.js -D using_registry -D debug_level=ERROR -main Main -dce full
 mocha spec/runTests.js -t 5000
